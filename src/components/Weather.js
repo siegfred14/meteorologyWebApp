0d35b306a64c53127d88128d55bcaf1d0aef4084
Siegfred Samson <siegfredsamson@gmail.com>
2022-06-28 20:13:16 +0100

    weather app set up complete

---
+++ src/components/Weather.js
@@ -0,0 +1,10 @@
+import React, { useState } from "react";
+import axios from "axios";
+
+export const Weather = () => {
+  return (
+    <div>
+      <h1>Weather Placeholder</h1>
+    </div>
+  );
+};
